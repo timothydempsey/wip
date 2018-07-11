e29ceb21736fd897bba198b50cfd5fa1bc6b558b
Timothy Dempsey <tim@dempseystudio.com>
2018-07-11 08:17:28 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import painting from './gipsy.jpg';
+import './App.css';
+import Footer from './Footer.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+
+
+        <header className="App-header">
+        <h1>DEMPSEYSTUDIO</h1>
+            <h2 className="tagline">Work(s) In Progress</h2>
+        </header>
+
+        <img src={painting} className="heroImage" alt="Portrait" />
+
+</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+	render() {
+		return (
+		<div className="footer_style">
+			Legal stuff here
+		</div>
+		)
+	}
+}
+
+export default Footer
