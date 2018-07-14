c984bcbdca6b4afa25be3351d5ef7424dde9d858
timothydempsey <tim@dempseystudio.com>
2018-07-14 15:17:28 -0400

    New edits, removal of placeholder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import painting from './nyack-life-1-dog.jpg';
+import sketch from './nyack-homes-1-sketch.jpg';
 import './App.css';
 //import Footer from './Footer.js'
 
@@ -15,9 +16,21 @@ class App extends Component {
         <h1>DEMPSEYSTUDIO</h1>
             <h2 className="tagline">Work(s) In Progress</h2>
         </header>
+        <div>
+        <h3>A Boy and his Dog</h3>
+        <p>
+        This oil-on-canvas is stalled a little longer than I would like.
+        Estimated another 8 - 10 hours and I can call it complete.
+        </p>
+        <img src={painting} className="heroImage" alt="Oil Painting" />
 
-        <img src={painting} className="heroImage" alt="Portrait" />
+        <h3>Rooftops of Nyack (Working Title)</h3>
+        <p>
+        This study is the basis of a larger piece, which may or may not be executed in pastel on paper. I reserve the right to change my mind on the medium. May go with oil or acrylic paint on canvas.
+        </p>
+        <img src={sketch} className="heroImage" alt="Sketch" />
 
+        </div>
 </div>
     );
   }
